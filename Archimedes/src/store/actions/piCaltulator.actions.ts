323a59ac781ec54a8d5923aa903670e8ffcd0812
NisanurBulut <nbulut@sarplojistik.com>
2021-01-24 18:29:14 +0300

    defined effect actions for pi items

--- Archimedes/src/store/actions/piCaltulator.actions.ts
+++ Archimedes/src/store/actions/piCaltulator.actions.ts
@@ -2,16 +2,50 @@ import { Action } from '@ngrx/store';
 import { PiItem } from '../models/pi-item.model';
 
 export enum PiCalculatorActionTypes {
+    LOAD_ITEMS = '[PiCalculator] Load Pi Items',
+    LOAD_ITEMS_SUCCESS = '[PiCalculator] Load Pi Items Success',
+    LOAD_ITEMS_ERROR = '[PiCalculator] Load Pi Items Error',
     ADD_ITEM = '[PiCalculator] Add Item',
-    DELETE_ITEM = '[PiCalculator] Delete Item'
+    ADD_ITEM_SUCCESS = '[PiCalculator] Add Item Success',
+    ADD_ITEM_ERROR = '[PiCalculator] Add Item Error',
+    DELETE_ITEM = '[PiCalculator] Delete Item',
+    DELETE_ITEM_SUCCESS = '[PiCalculator] Delete Item Success',
+    DELETE_ITEM_ERROR = '[PiCalculator] Delete Item Error'
+}
+export class LoadPiItemsAction implements Action {
+    readonly type: string = PiCalculatorActionTypes.LOAD_ITEMS;
+    constructor(public payload: Array<PiItem>) { }
+}
+export class LoadPiItemsSuccessAction implements Action {
+    readonly type: string = PiCalculatorActionTypes.LOAD_ITEMS_SUCCESS;
+    constructor(public payload: Array<PiItem>) { }
+}
+export class LoadPiItemsErrorAction implements Action {
+    readonly type: string = PiCalculatorActionTypes.LOAD_ITEMS_ERROR;
+    constructor(public payload: string) { }
 }
 export class AddItemAction implements Action {
     readonly type: string = PiCalculatorActionTypes.ADD_ITEM;
     constructor(public payload: PiItem) { }
 }
+export class AddItemSuccessAction implements Action {
+    readonly type: string = PiCalculatorActionTypes.ADD_ITEM_SUCCESS;
+    constructor(public payload: PiItem) { }
+}
 export class DeleteItemAction implements Action {
-    readonly type:string=PiCalculatorActionTypes.DELETE_ITEM;
-    constructor(public payload:PiItem){}
+    readonly type: string = PiCalculatorActionTypes.DELETE_ITEM;
+    constructor(public payload: PiItem) { }
+}
+export class DeleteItemSuccessAction implements Action {
+    readonly type: string = PiCalculatorActionTypes.DELETE_ITEM_SUCCESS;
+    constructor(public payload: string) { }
 }
 
-export type PiItemAction = AddItemAction | DeleteItemAction;
+
+export type PiItemAction = AddItemAction
+    | AddItemSuccessAction
+    | DeleteItemAction
+    | DeleteItemSuccessAction
+    | LoadPiItemsAction
+    | LoadPiItemsErrorAction
+    | LoadPiItemsSuccessAction;
--- Archimedes/src/store/reducers/piCalculator.reducer.ts
+++ Archimedes/src/store/reducers/piCalculator.reducer.ts
@@ -1,13 +1,42 @@
-import { PiItemAction, PiCalculatorActionTypes } from "../actions/piCaltulator.actions";
-import { PiItem } from "../models/pi-item.model";
+import { PiItemAction, PiCalculatorActionTypes } from '../actions/piCaltulator.actions';
+import { PiItem } from '../models/pi-item.model';
 
-const initialState: Array<PiItem> = [];
-export function PiItemReducer(state: Array<PiItem> = initialState, action: PiItemAction) {
+export interface PiItemsState {
+    list: PiItem[];
+    loading: boolean;
+    error: Error;
+}
+const initialState: PiItemsState = {
+    list: [],
+    loading: false,
+    error: null
+};
+export function PiItemReducer(state: PiItemsState = initialState, action: PiItemAction) {
     switch (action.type) {
+        case PiCalculatorActionTypes.LOAD_ITEMS:
+            return { ...state, loading: true };
+        case PiCalculatorActionTypes.LOAD_ITEMS_SUCCESS:
+            return { ...state, list: action.payload, loading: false };
+        case PiCalculatorActionTypes.LOAD_ITEMS_ERROR:
+            return { ...state, error: action.payload, loading: false };
         case PiCalculatorActionTypes.ADD_ITEM:
-            return [...state, action.payload];
+            return { ...state, loading: true };
+        case PiCalculatorActionTypes.ADD_ITEM_SUCCESS:
+            return { ...state, list: [...state.list, action.payload], loading: false };
+        case PiCalculatorActionTypes.ADD_ITEM_ERROR:
+            return { ...state, error: action.payload, loading: false };
         case PiCalculatorActionTypes.DELETE_ITEM:
-            return state.filter(a => a.id != action.payload.id);
+            return {
+                ...state,
+                loading: true
+            };
+        case PiCalculatorActionTypes.DELETE_ITEM_SUCCESS:
+            return {
+                ...state,
+                list: state.list.filter(item => item.id !== action.payload), loading: false
+            };
+        case PiCalculatorActionTypes.DELETE_ITEM_ERROR:
+            return { ...state, error: action.payload, loading: false };
         default:
             return state;
     }
