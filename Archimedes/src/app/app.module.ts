1083b991ad4ee79e7143cc600fd7e339dbd66ca2
NisanurBulut <nbulut@sarplojistik.com>
2021-01-24 17:36:27 +0300

    defined pi-items component

--- Archimedes/src/app/app.module.ts
+++ Archimedes/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PiItemReducer } from '../store/reducers/piCalculator.reducer';
 import { FormsModule } from '@angular/forms';
+import { PiItemsComponent } from './pi-items/pi-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PiItemsComponent
   ],
   imports: [
     BrowserModule,
