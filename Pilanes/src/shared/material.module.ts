28ca4f8cca96f897a6d08bd241d0c54fb679363d
NisanurBulut <nbulut@sarplojistik.com>
2021-01-27 14:43:02 +0300

    installed angular material module

--- Pilanes/src/shared/material.module.ts
+++ Pilanes/src/shared/material.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
-
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button'
 @NgModule({
     declarations: [],
-    imports: [],
-    exports: []
+    imports: [
+        MatTabsModule,
+        MatButtonModule
+    ],
+    exports: [
+        MatTabsModule,
+        MatButtonModule
+    ]
 })
 export class MaterialModule {
 }
