71cdbac6dd537ea7551d4a1f350c93f5e7f1a0e2
NisanurBulut <nbulut@sarplojistik.com>
2021-02-21 15:38:44 +0300

    installed effect module

--- Lena/src/app/app.module.ts
+++ Lena/src/app/app.module.ts
@@ -6,11 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AlertConfig, AlertModule } from 'ngx-bootstrap/alert';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ImageDetailComponent } from './image/image-detail.component';
-import { ImageService } from './shared/image.service';
+import { GalleryService } from './shared/gallery.service';
 import { ImageFilterPipe } from './shared/filter.pipe';
 import { RouterModule } from '@angular/router';
 import { routes } from './app-routing.module';
-
+import { StoreModule } from '@ngrx/store';
+import { GalleryReducer } from './store/gallery.reducer';
+import { GalleryEffect } from './store/gallery.effects';
+import { EffectsModule } from '@ngrx/effects';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,10 +25,12 @@ import { routes } from './app-routing.module';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    StoreModule.forRoot({ gallery: GalleryReducer }),
+    EffectsModule.forRoot([GalleryEffect]),
     AlertModule.forRoot(),
     RouterModule.forRoot(routes)
   ],
-  providers: [AlertConfig, ImageService, ImageFilterPipe],
+  providers: [AlertConfig, GalleryService, ImageFilterPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
