822ecb486b74161a10ae8f7459912e503e15321a
NisanurBulut <nbulut@sarplojistik.com>
2021-02-21 15:19:54 +0300

    implemented gallery actions

---
+++ Lena/src/app/store/gallery.actions.ts
@@ -0,0 +1,39 @@
+import { Action } from '@ngrx/store';
+import { Image } from '../shared/image.model';
+export enum GaleryActionTypes {
+    LOAD_GALLERY = '[Galery] Load Galery',
+    LOAD_GALLERY_SUCCESS = '[Galery] Load Galery Success',
+    LOAD_GALLERY_ERROR = '[Galery] Load Galery Error',
+    LOAD_IMAGE = '[Galery] Load IMAGE',
+    LOAD_IMAGE_SUCCESS = '[Galery] Load IMAGE Success',
+    LOAD_IMAGE_ERROR = '[Galery] Load IMAGE Error',
+}
+export class LoadGalleryAction implements Action {
+    readonly type = GaleryActionTypes.LOAD_GALLERY;
+}
+export class LoadGallerySuccessAction implements Action {
+    readonly type = GaleryActionTypes.LOAD_GALLERY_SUCCESS;
+    constructor(public payload: Array<Image>) { }
+}
+export class LoadGalleryErrorAction implements Action {
+    readonly type = GaleryActionTypes.LOAD_GALLERY_ERROR;
+    constructor(public payload: Error) { }
+}
+export class LoadImageAction implements Action {
+    readonly type: string = GaleryActionTypes.LOAD_IMAGE;
+    constructor(public payload: string) { }
+}
+export class LoadImageSuccessAction implements Action {
+    readonly type: string = GaleryActionTypes.LOAD_IMAGE_SUCCESS;
+    constructor(public payload: string) { }
+}
+export class LoadImageErrorAction implements Action {
+    readonly type: string = GaleryActionTypes.LOAD_IMAGE_ERROR;
+    constructor(public payload: string) { }
+}
+export type GalleryActionTypes = LoadGalleryAction |
+    LoadGalleryErrorAction |
+    LoadGallerySuccessAction |
+    LoadImageAction |
+    LoadImageErrorAction |
+    LoadImageSuccessAction;
