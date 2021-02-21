ccd0f70435e3d17ddbfa1de6769852bdff394244
NisanurBulut <nbulut@sarplojistik.com>
2021-02-21 16:23:30 +0300

    dispatch image load

--- Lena/src/app/image/image-detail.component.ts
+++ Lena/src/app/image/image-detail.component.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromGalleryActionTypes from '../store/gallery.actions';
+import { AppState } from '../shared/app.reducer';
 import { ActivatedRoute } from '@angular/router';
-import { ImageService } from '../shared/image.service';
-
+import { Image } from '../shared/image.model';
 @Component({
   selector: 'app-image-detail',
   templateUrl: './image-detail.component.html',
   styleUrls: ['./image-detail.component.css']
 })
 export class ImageDetailComponent implements OnInit {
-  image: any;
-  constructor(private imageService: ImageService,
-              private route: ActivatedRoute) { }
+  image: Image;
+  constructor(private store: Store<AppState>,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.image = this.imageService.getImage(
-      +this.route.snapshot.params['id']);
+    this.store.dispatch(new fromGalleryActionTypes.LoadImageAction(+this.route.snapshot.params['id']))
   }
 
 }
