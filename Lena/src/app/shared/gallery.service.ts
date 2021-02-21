5ea3c70a429e3a1ad6633cbf57be09a558258cbd
NisanurBulut <nbulut@sarplojistik.com>
2021-02-21 16:23:56 +0300

    implemented gallery service

---
+++ Lena/src/app/shared/gallery.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Image } from './image.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GalleryService {
+    constructor(private _http: HttpClient) { }
+    getGallery() {
+        return this._http.get<Array<Image>>(environment.apiImage);
+    }
+    getImage(id: number) {
+        return this._http.get(`${environment.apiImage}/${id}`);
+    }
+}
