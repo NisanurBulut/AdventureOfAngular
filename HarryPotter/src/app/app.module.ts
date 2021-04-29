22290b81102a2131c847f44cf503abd1ec0df4a3
Nisanur BULUT <nbulut@sarplojistik.com>
2021-04-29 20:37:56 +0300

    created sidebar and context components

--- HarryPotter/src/app/app.module.ts
+++ HarryPotter/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
