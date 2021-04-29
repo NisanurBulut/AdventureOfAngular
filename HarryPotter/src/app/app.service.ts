612c841def92bc625141efc39dec68afef83741a
Nisanur BULUT <nbulut@sarplojistik.com>
2021-04-30 00:59:45 +0300

    implemented service

--- HarryPotter/src/app/app.service.ts
+++ HarryPotter/src/app/app.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 }
