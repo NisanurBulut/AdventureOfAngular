7b25f100555b83dd19843b4ba0f6fdeaa7dcfd12
Nisanur BULUT <nbulut@sarplojistik.com>
2021-04-30 00:38:20 +0300

    created components

---
+++ HarryPotter/src/app/content/character/character.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-character',
+  templateUrl: './character.component.html',
+  styleUrls: ['./character.component.css']
+})
+export class CharacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
