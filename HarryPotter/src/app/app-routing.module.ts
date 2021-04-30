f48b8e33cb9903a79f46f7841669b37b583b7b39
Nisanur BULUT <nbulut@sarplojistik.com>
2021-04-30 13:08:48 +0300

    fixed app routing

--- HarryPotter/src/app/app-routing.module.ts
+++ HarryPotter/src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { StaffComponent } from './components/staff/staff.component';
 import { StudentsComponent } from './components/students/students.component';
 
 const routes: Routes = [
-  { path: '**', component: CharactersComponent },
   { path: 'characters', component: CharactersComponent },
   { path: 'students', component: StudentsComponent },
-  { path: 'staff', component: StaffComponent }
+  { path: 'staff', component: StaffComponent },
+  { path: '**', component: CharactersComponent }
 ];
 
 @NgModule({
